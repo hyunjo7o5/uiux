420a0ebb4fbfb1b77a6057e04df8939757044abe
hyunjo7o5 <nhj19111@gmail.com>
2018-05-21 18:00:34 +0900

    gw
    gw

--- study/yg page/js/winner.js
+++ study/yg page/js/winner.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
 
+	$(".menu").on("click",function(){
+		
+		$(".menu").toggleClass("on");
+		
+		$(".nav").toggle();
+		
+		console.log("클릭");
+		
+	});
+	
+
+	
+
 });
